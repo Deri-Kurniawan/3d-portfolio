262e22943730d5ab19cec6a5edffaed47893b32a
Deri Kurniawan <deri.netuchi@gmail.com>
2023-05-17 02:08:34 +0700

    feat: setup project configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,26 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: "#A293FF",
+        secondary: "#00F0FF",
+        accent: "#000000",
+        accent2: "#8E8E8E",
+        gray: "#F1F1F1",
       },
+    },
+    fontFamily: {
+      montserrat: ["var(--font-montserrat)", ...fontFamily.sans],
+      poppins: ["var(--font-poppins)", ...fontFamily.sans],
     },
   },
   plugins: [],
-}
+};
