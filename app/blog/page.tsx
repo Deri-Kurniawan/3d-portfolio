8ba5bd89f87353f74fecc27a5f2f2b8b0c856eb0
Deri Kurniawan <deri.netuchi@gmail.com>
2023-05-17 02:28:41 +0700

    feat: add pages - add page home, blog and project

---
+++ app/blog/page.tsx
@@ -0,0 +1,10 @@
+export default function Blog() {
+    return (
+        <div className="safe-layout">
+            <div className='text-center'>
+                <h2 className='mb-6 text-5xl font-extrabold lg:text-6xl font-montserrat gradient-text'>Blog</h2>
+                <p className='font-medium text-xl lg:text-2xl text-accent max-w-[960px] mx-auto'>Under Development</p>
+            </div>
+        </div>
+    )
+}
