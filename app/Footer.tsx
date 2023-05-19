65a619a6532b9e11a7d8dc4fa8ce66fa3e55a35a
Deri Kurniawan <deri.netuchi@gmail.com>
2023-05-19 16:54:25 +0700

    feat: update dynamic year

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <footer>
             <div className='py-6 text-center safe-x-padding'>
-                <p className='text-xl font-medium lg:text-2xl'>©2023 Deri Kurniawan All rights reserved.</p>
+                <p className='text-xl font-medium lg:text-2xl'>©{new Date().getFullYear()} Deri Kurniawan All rights reserved.</p>
             </div>
         </footer>
     )
